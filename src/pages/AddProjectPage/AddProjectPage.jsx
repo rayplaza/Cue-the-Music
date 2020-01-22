a684f10949b5c0efd414abf8143573b813360033
Ray Plaza <munditoplaza@gmail.com>
2020-01-21 16:29:57 -0800

    working on Add Project Page

--- src/pages/AddProjectPage/AddProjectPage.jsx
+++ src/pages/AddProjectPage/AddProjectPage.jsx
@@ -15,6 +15,46 @@ class AddProjectPage extends Component {
   };
 
   formRef = React.createRef();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleAddProject(this.state.formData);
+  };
+
+  handleChange = e => {
+    const formData = {
+      ...this.state.formData,
+      [e.target.name]: e.target.value
+    };
+    this.setState({
+      formData,
+      invalidForm: !this.formRef.current.checkValidity()
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Add Project</h1>
+        <form
+          ref={this.formRef}
+          autoComplete="off"
+          onSubmit={this.handleSubmit}
+        >
+          <div className="form-group">
+            <label>Project Name (required)</label>
+            <input
+              className="form-control"
+              projectTitle="Name of Project"
+              value={this.state.formData.projectTitle}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+        </form>
+      </>
+    );
+  }
 }
 
 export default AddProjectPage;
