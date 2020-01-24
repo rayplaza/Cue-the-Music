ecee2bb28b8970721724d7df6c6f24d77bb48df3
Ray Plaza <munditoplaza@gmail.com>
2020-01-23 16:29:18 -0800

    updated styling on main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./MainPage.css";
+import Background from '../../img/Musicforfilm.jpg';
+import Logo from '../../img/logotransback.png';
+
+var sectionStyle = {
+  width: "100%",
+  height: "100vh",
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover'
+}
+
 
 const MainPage = props => {
-  return <div className="MainPage">Main Page</div>;
+  return (
+    <section style={sectionStyle}>
+      <img className="logo" src={Logo} alt="the logo" />
+    </section>
+  )
 };
 
 export default MainPage;
